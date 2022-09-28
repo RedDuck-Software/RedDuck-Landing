79356643c2429ae7c1307262f8bca457d5009da6
Kiryl Knysh <10k20.dev@gmail.com>
2022-09-28 23:04:49 +0300

    feat: header adaptive layout

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Texture } from './../shared/Texture';
+import { ReactComponent as LogoHorizontal } from './../../assets/img/logo-horizontal.svg';
+import { ReactComponent as InstagramIcon } from './../../assets/img/icons/inst-icon.svg';
+import { ReactComponent as FacebookIcon } from './../../assets/img/icons/fb-icon.svg';
+import { ReactComponent as LinkedinIcon } from './../../assets/img/icons/linkedin-icon.svg';
+import { ReactComponent as TwitterIcon } from './../../assets/img/icons/twitter-icon.svg';
 import './Header.scss';
  
 export const Header = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const openMenu = () => {
+    setIsMenuOpened(!isMenuOpened);
+    let htmlTag = document.querySelector('html');
+    isMenuOpened ? htmlTag!.style.overflowY = 'auto' : htmlTag!.style.overflowY = 'hidden'
+  }
   return (
     <header>
-      <Texture textureStyles={{left: '-1050px', top: '-1000px', transform: 'scale(0.5)'}}/>
+      <Texture className="texture" textureStyles={{left: '-1050px', top: '-1000px', transform: 'scale(0.5)'}}/>
+      <LogoHorizontal className='header-logo'/>
       <nav>
         <li><Link to="/expertise">Expertise</Link></li>
         <li><Link to="/services">Services</Link></li>
@@ -18,6 +30,40 @@ export const Header = () => {
         <li><Link to="/about">About us</Link></li>
         <li className='lang-changer'>En</li>
       </nav>
+      <div onClick={openMenu} className={isMenuOpened ? 'header-menu-icon active' : 'header-menu-icon'}>
+        <div className="rectangle"></div>
+        <div className="rectangle"></div>
+        <div className="rectangle"></div>
+      </div>
+      <div className={isMenuOpened ? 'header-menu active' : 'header-menu'}>
+        <ul>
+          <li><Link to="/expertise">Expertise</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <hr/>
+          <li><Link to="/cases">Cases</Link></li>
+          <li><Link to="/career">Career</Link></li>
+          <hr/>
+          <li><Link to="/education">Education</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <hr/>
+          <li><Link to="/contacts">Contact us</Link></li>
+          <li><Link to="/about">About us</Link></li>
+        </ul>
+        <div className="header-menu-icons">
+          <div className="header-menu-icons-icon">
+            <InstagramIcon />
+          </div>
+          <div className="header-menu-icons-icon">
+            <FacebookIcon />
+          </div>
+          <div className="header-menu-icons-icon">
+            <TwitterIcon />
+          </div>
+          <div className="header-menu-icons-icon">
+            <LinkedinIcon />
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
