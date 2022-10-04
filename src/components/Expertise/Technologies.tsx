ca3078f58fba5e8c9483a1c33700f6da84528167
Kiryl Knysh <10k20.dev@gmail.com>
2022-10-04 15:19:45 +0300

    fixes

--- src/components/Expertise/Technologies.tsx
+++ src/components/Expertise/Technologies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { SectionTitle } from "../shared/SectionTitle";
 import "./Technologies.scss";
 import { Texture } from "../shared/Texture";
